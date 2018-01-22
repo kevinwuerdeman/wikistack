aae43c5d503f03736d2da390b5688f79734c2518
Kevin Wuerdeman <kevinwuerdeman@gmail.com>
2018-01-22 14:38:56 -0600

    content function added

--- app.js
+++ app.js
@@ -5,9 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 const nunjucks = require('nunjucks');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
+const models = require('./models')
+const index = require('./routes');
 
 var app = express();
 
@@ -17,10 +16,6 @@ app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', nunjucks.render); // when res.render works with html files, have it use nunjucks to do so
 
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -29,15 +24,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
 
 // error handler
 app.use(function(err, req, res, next) {
@@ -49,5 +44,12 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+models.db.sync({force: true})
+.then(function () {
+  app.listen(3000, () => console.log('App listening on 3000'));
+})
+.catch(console.error);
 
 module.exports = app;
--- models/index.js
+++ models/index.js
@@ -1,32 +1,63 @@
 const Sequelize = require('sequelize');
-const db =  new Sequelize('postgres://localhost:5432/wikistack');
-
+const db =  new Sequelize('postgres://localhost:5432/wikistack', {
+  logging: false
+});
 
 const Page = db.define('page', {
   title: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
+    allowNull: false
   },
   urlTitle: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
+    allowNull: false
   },
   content: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
+    allowNull: false
   },
-  status: Sequelize.ENUM('open', 'closed')
+  status: {
+    type: Sequelize.ENUM('open', 'closed')
+  },
+  date: {
+    type: Sequelize.DATE,
+    defaultValue: Sequelize.NOW
+  }
+}, {
+  getterMethods: {
+    route() {
+      console.log(this.urlTitle)
+      return '/wiki/' + this.urlTitle;
+    }
+  },
+  hooks: {
+    beforeValidate: (page) => {
+      if (page.title) {
+      page.urlTitle = page.title.replace(/\s+/g, '_').replace(/\W/g, '');
+    } else {
+      page.urlTitle = Math.random().toString(36).substring(2, 7);
+    }
+    }
+  }
 });
 
 const User = db.define('user', {
   name: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
+    allowNull: false
   },
   email: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true
+    }
   }
 
 });
 
-
 module.exports = {
   Page: Page,
-  User: User
+  User: User,
+  db: db
 };
