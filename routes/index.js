c0dc6caad3cb4722187782f727dd610a69d7b6a7
Kevin Wuerdeman <kevinwuerdeman@gmail.com>
2018-01-22 14:36:50 -0600

    content pages setup

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,19 @@ var express = require('express');
 var router = express.Router();
 const userRouter = require('./user');
 const wikiRouter = require('./wiki')
+const models = require('../models');
+const Page = models.Page;
+const User = models.User;
 
 router.use('/users', userRouter);
 router.use('/wiki', wikiRouter);
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  Page.findAll({
+
+  }).then(page => res.render('index', {pages: page}))
+  //res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
--- routes/wiki.js
+++ routes/wiki.js
@@ -17,11 +17,21 @@ router.post('/', function(req, res, next) {
   });
 
   page.save()
-  .then(res.json(page));
+  .then( page => res.redirect(page.route))
+  .catch(next);
 });
 
 router.get('/add', function(req, res, next) {
   res.render('addpage');
+});
+
+router.get('/:urlTitle', function(req, res, next) {
+  Page.findOne({
+    where: {
+      urlTitle: req.params.urlTitle
+    }
+  }).then( page => res.render('wikipage', {page: page}))
+  .catch(next);
 });
 
 module.exports = router;
